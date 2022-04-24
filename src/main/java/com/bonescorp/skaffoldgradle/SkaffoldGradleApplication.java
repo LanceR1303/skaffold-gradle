d4bff8af1c7e18a213a5c9d5c75cc632d8ffdca4
lance <lancerodrigues13@gmail.com>
2022-04-24 12:35:39 +1000

    Test Git Push Hook

---
+++ src/main/java/com/bonescorp/skaffoldgradle/SkaffoldGradleApplication.java
@@ -0,0 +1,13 @@
+package com.bonescorp.skaffoldgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonescorp/skaffoldgradle/SkaffoldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonescorp.skaffoldgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
